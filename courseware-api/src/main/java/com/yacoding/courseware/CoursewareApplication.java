6f92e3f5fc4d8a6a032e151a06a8282cdb27c4b5
yxym0203 <1500081802@qq.com>
2022-07-26 21:48:33 +0800

    springboot+vue+小程序项目实战

---
+++ courseware-api/src/main/java/com/yacoding/courseware/CoursewareApplication.java
@@ -0,0 +1,17 @@
+package com.yacoding.courseware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author yaCoding
+ * @create 2022-07-26 下午 9:39
+ */
+@SpringBootApplication
+public class CoursewareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursewareApplication.class, args);
+    }
+
+}
---
+++ courseware-api/src/main/java/com/yacoding/courseware/pojo/Courseware.java
@@ -0,0 +1,9 @@
+package com.yacoding.courseware.pojo;
+
+/**
+ * @Author yaCoding
+ * @create 2022-07-26 下午 9:42
+ */
+
+public class Courseware {
+}
